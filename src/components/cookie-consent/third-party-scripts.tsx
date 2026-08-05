ce669af798cda5a3dd5cb3be98e7167a3378d8be
bahlrulez <officialdeepbahl@gmail.com>
2026-08-05 13:25:56 +0530

    chore: configure Google Analytics ID

--- src/components/cookie-consent/third-party-scripts.tsx
+++ src/components/cookie-consent/third-party-scripts.tsx
@@ -15,14 +15,14 @@ export function ThirdPartyScripts() {
         <>
           <Script 
             strategy="lazyOnload"
-            src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX" 
+            src="https://www.googletagmanager.com/gtag/js?id=G-49NFK7K9W6" 
           />
           <Script id="google-analytics" strategy="lazyOnload">
             {`
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-XXXXXXXXXX', {
+              gtag('config', 'G-49NFK7K9W6', {
                 page_path: window.location.pathname,
               });
             `}
